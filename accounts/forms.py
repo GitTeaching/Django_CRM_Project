af675b4e049cbe737e83bcbc1a4f752ab278dfca
Aicha Aid <aid_aicha@hotmail.fr>
2020-11-18 09:49:06 +0100

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
-
+# Order Form
 class OrderForm(ModelForm):
 	class Meta:
 		model = Order
